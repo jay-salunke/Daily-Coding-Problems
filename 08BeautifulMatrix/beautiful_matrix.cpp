724e32fa292b061d49e430e9d2d73915b373e247
Jay-Salunke <jysl2004@gmail.com>
2022-02-19 15:14:58 +0530

    refactor beautiful matrix code

--- 08BeautifulMatrix/beautiful_matrix.cpp
+++ 08BeautifulMatrix/beautiful_matrix.cpp
@@ -39,16 +39,16 @@ int main()
 
     int matrix[5][5] = {
         {0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 1},
         {0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0}};
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+    };
 
     int k, m;
-
+    bool found = false;
     for (int i = 0; i < 5; i++)
     {
-        bool found = false;
         for (int j = 0; j < 5; j++)
         {
             if (matrix[i][j] == 1)
@@ -65,26 +65,32 @@ int main()
     }
 
     int result = 0;
+
+    if (!found)
+    {
+        cout << "one is not there";
+        exit(-1);
+    }
     if (k > 2)
     {
         if (m > 2)
         {
-            result = k - 2 + m - 2;
+            result = k + m - 4;
         }
         else
         {
-            result = k - 2 + 2 - m;
+            result = k - m;
         }
     }
     else if (k < 2)
     {
         if (m > 2)
         {
-            result = 2 - k + m - 2;
+            result = k + m - 2;
         }
         else
         {
-            result = 2 - k + 2 - m;
+            result = k + 2 - m;
         }
     }
     else if (k == 2)
